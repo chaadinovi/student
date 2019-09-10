654e27b6bf2ae3fd9176e37a318f8e6ab5553307
Chaadi Idrissi <chaadimarkets@gmail.com>
2019-09-10 10:28:15 +0200

    tictactoe

--- src/main/java/com/mycompany/gittest/Main.java
+++ src/main/java/com/mycompany/gittest/Main.java
@@ -12,7 +12,7 @@ package com.mycompany.gittest;
 public class Main {
     public static void main(String[] args) {
         
-        System.out.println("hello world");
+        System.out.println("hello tictactoe");
         
     }
     
